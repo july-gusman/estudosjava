d6a3522ea15a36d47f0152022a7549499ed34eb5
july-gusman <julianagusman95@gmail.com>
2024-05-27 15:59:59 -0300

    Estudos sobre controle de fluxo e sobre estruturas de repetição

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,13 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        
+        //Em arrays o indice inicia no zero
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        //Forma abreviada
+	for(String aluno : alunos) {
+	  System.out.println("Nome do aluno é: " + aluno);
+	}
+
+    }
+}
